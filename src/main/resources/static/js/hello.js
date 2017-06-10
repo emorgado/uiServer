4736d2aac8637930c5ffd89c5a548b59dd6a7418
Emerson Jose Morgado Brito <emerson.morgado@gmail.com>
2017-06-10 16:31:48 -0300

    Correção da navegaçaõ

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -8,7 +8,7 @@
 		.controller('navigation', navigation)
 	;
 
-	function config($routeProvider, $httpProvider) {
+	function config($routeProvider, $httpProvider, $locationProvider ) {
 		$routeProvider.when('/', {
 			templateUrl : 'home.html',
 			controller : 'home',
@@ -18,7 +18,11 @@
 			controller : 'navigation',
 			controllerAs : 'controller'
 		}).otherwise('/');
-
+		$locationProvider.hashPrefix('');
+//		$locationProvider.html5Mode({
+//            enabled: true,
+//            requireBase: true
+//        });
 		$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 	}
 
@@ -35,6 +39,65 @@
 		});
 	}
 
+	function navigation($rootScope, $http, $location, $route) {
+		var self = this;
+
+		self.tab = function(route) {
+			return $route.current && route === $route.current.controller;
+		};
+
+		var authenticate = function(credentials, callback) {
+
+			var headers = credentials ? {
+				authorization : "Basic "
+					+ btoa(credentials.username + ":"
+						+ credentials.password)
+			} : {};
+
+			$http.get('user', {
+				headers : headers
+			}).then(function(response) {
+				if (response.data.name) {
+					$rootScope.authenticated = true;
+				} else {
+					$rootScope.authenticated = false;
+				}
+				callback && callback();
+			}, function() {
+				$rootScope.authenticated = false;
+				callback && callback();
+			});
+
+		}
+
+		authenticate();
+
+		self.credentials = {};
+		self.login = function() {
+			authenticate(self.credentials, function() {
+				if ($rootScope.authenticated) {
+					console.log("Login succeeded")
+					$location.path("/");
+					self.error = false;
+					$rootScope.authenticated = true;
+				} else {
+					console.log("Login failed")
+					$location.path("/login");
+					self.error = true;
+					$rootScope.authenticated = false;
+				}
+			})
+		};
+
+		self.logout = function() {
+			$http.post('logout', {}).finally(function() {
+				$rootScope.authenticated = false;
+				$location.path("/");
+			});
+		}
+
+	}
+	/*
 	function navigation($rootScope, $http, $location, $log, $route) {
 		$log.info("navigation");
 		var self = this;
@@ -91,5 +154,6 @@
 			});
 		}
 	}
+	*/
 
 })(angular);
